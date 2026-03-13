caabcc5e284de629edf93cc01576c3cf4a868cba
shinsu0681 <sinseungsu02@gmail.com>
2026-03-13 10:48:55 +0900

    feat : 개발 기록 블로그 기능구현

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-# app.py
-
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+logs = []
+
 @app.route("/")
-def index():
-    return "hi yo"
+def home():
+    return render_template("home.html", logs=logs)
+
+
+@app.route("/write", methods=["GET", "POST"])
+def write():
+    if request.method == "POST":
+        log_type = request.form["type"]
+        title = request.form["title"]
+        content = request.form["content"]
+
+        log = {
+            "id": len(logs),
+            "type": log_type,
+            "title": title,
+            "content": content
+        }
+
+        logs.append(log)
+        return redirect("/")
+
+    return render_template("write.html")
+
+
+@app.route("/log/<int:id>")
+def log_detail(id):
+    log = logs[id]
+    return render_template("log.html", log=log)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
